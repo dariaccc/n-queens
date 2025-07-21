ed2347a0c0254f4cb4c113d373af3f6848f4ecbe
dasha <daria.carlberg@gmail.com>
2025-07-21 16:37:45 +0200

    working visualisation

--- visualisation.py
+++ visualisation.py
@@ -3,7 +3,18 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QLa
 from PyQt5.QtGui import QColor, QPalette, QPixmap
 #from PyQt5.QtSvg import QSvgWidget
 
-nr_queens = 12
+nr_queens = 8
+size = 52 - nr_queens
+#window_size = nr_queens * 50
+list = [2, 7, 3, 6, 8, 5, 1, 4]
+
+for i in list:
+    i = 5
+
+for idx, item in enumerate(list):
+    list[idx] = item + idx*nr_queens - 1
+
+print(list)
 
 class Color(QWidget):
     def __init__(self, color):
@@ -17,7 +28,7 @@ class Color(QWidget):
 class Queen(QLabel):
     def __init__(self):
         super().__init__()
-        pixmap = QPixmap('images/queen.svg')
+        pixmap = QPixmap('images/queen.svg').scaled(size,size)
         self.setPixmap(pixmap)
 
 def createhorisontal():
@@ -25,6 +36,7 @@ def createhorisontal():
     layout.setHorizontalSpacing(0)
     layout.setVerticalSpacing(0)
     x = 0
+    counter = 0
     for i in range(0, nr_queens):
         if i % 2 == 0:
             first = "#23461a"
@@ -34,11 +46,16 @@ def createhorisontal():
             second = "#23461a"
         for _ in range(0, int(nr_queens/2)):
             layout.addWidget(Color(first), i, x)
-            layout.addWidget(Queen(), i, x)
+            if counter in list:
+                layout.addWidget(Queen(), i, x)
             x = x+1
+            counter += 1
             print(x)
             layout.addWidget(Color(second), i, x)
+            if counter in list:
+                layout.addWidget(Queen(), i, x)
             x = x+1
+            counter += 1
             if x >= nr_queens:
                 x = 0
     return layout
